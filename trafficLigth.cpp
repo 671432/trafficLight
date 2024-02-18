9c9f2a8b5fa7c8ddeee8cf6b73679fcbf66cbb38
Justin <111425385+671432@users.noreply.github.com>
2024-02-18 23:01:46 +0100

    Done

--- trafficLigth.cpp
+++ trafficLigth.cpp
@@ -3,35 +3,30 @@
 
 #include "framework.h"
 #include "trafficLigth.h"
+#include <deque>
+#include <strsafe.h>
 #include <Windows.h>
 #include <memory>
-#include <Math.h>
 
 
 #define MAX_LOADSTRING 100
-#define _USE_MATH_DEFINES
 #define TIMER_LYS 1
-#define TIMER_BIL 3
-#define TIMER_ADD_BILER 4
-#define TIMER_DRIVE 6
-/*
-#define bilLengde 25
-#define bilBredde 50
+#define TIMER_BIL 2
+#define TIMER_ADD_BILER 3
+#define bilLengde 60
+#define bilBredde 30
 #define roadWidth 100
-*/
+
 
 // Global Variables:
 HINSTANCE hInst;                                // current instance
 WCHAR szTitle[MAX_LOADSTRING];                  // The title bar text
 WCHAR szWindowClass[MAX_LOADSTRING];            // the main window class name
 std::unique_ptr<TrafikkLys> trafikkLys[2];
-/*
 std::deque<COLORREF> northCarsColor;
 std::deque<int> northCarsPosition;
 std::deque<COLORREF> westCarsColor;
 std::deque<int> westCarsPosition;
-*/
-int lightstate = 0;
 int pW = 100;
 int pN = 100;
 
@@ -133,8 +128,6 @@ BOOL InitInstance(HINSTANCE hInstance, int nCmdShow)
 void DrawRoads(HDC hdc, RECT clientRect) {
     HBRUSH hBrush = CreateSolidBrush(RGB(210, 173, 128));
     HGDIOBJ hOrg = SelectObject(hdc, hBrush);
-    int roadWidth = 50; // Anta at dette er bredden på veien.
-    int spacing = 100; // Mellomrommet mellom veiene i krysset.
 
     // Beregner sentrumspunktet i vinduet
     int centerX = (clientRect.right) / 2;
@@ -142,16 +135,16 @@ void DrawRoads(HDC hdc, RECT clientRect) {
 
     // Tegner veiene i hvert hjørne
     // Øvre venstre vei
-    Rectangle(hdc, 0, 0, centerX - spacing / 2, centerY - spacing / 2);
+    Rectangle(hdc, 0, 0, centerX - roadWidth / 2, centerY - roadWidth / 2);
 
     // Øvre høyre vei
-    Rectangle(hdc, centerX + spacing / 2, 0, clientRect.right, centerY - spacing / 2);
+    Rectangle(hdc, centerX + roadWidth / 2, 0, clientRect.right, centerY - roadWidth / 2);
 
     // Nedre venstre vei
-    Rectangle(hdc, 0, centerY + spacing / 2, centerX - spacing / 2, clientRect.bottom);
+    Rectangle(hdc, 0, centerY + roadWidth / 2, centerX - roadWidth / 2, clientRect.bottom);
 
     // Nedre høyre vei
-    Rectangle(hdc, centerX + spacing / 2, centerY + spacing / 2, clientRect.right, clientRect.bottom);
+    Rectangle(hdc, centerX + roadWidth / 2, centerY + roadWidth / 2, clientRect.right, clientRect.bottom);
 
     SelectObject(hdc, hOrg);
     DeleteObject(hBrush);
@@ -160,31 +153,23 @@ void DrawRoads(HDC hdc, RECT clientRect) {
 
 LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
 {
-    
-    static int rot = 0;
-    static int circleX = 0;
-    
+    RECT clientRect;
+    GetClientRect(hWnd, &clientRect); // Hent størrelsen på vinduets klientområde
+    POINT center = { clientRect.right / 2, clientRect.bottom / 2 };
+
     switch (message)
     {
     case WM_CREATE:
     {
-        RECT clientRect;
-        GetClientRect(hWnd, &clientRect); // Hent størrelsen på vinduets klientområde
-
 
         // Definerer størrelser og plassering basert på klientområdet
-        int roadWidth = 50; // Bredden på veien.
-        int spacing = 100; // Mellomrommet mellom veiene i krysset.
-        int centerX = (clientRect.right) / 2;
-        int centerY = (clientRect.bottom) / 2;
+        trafikkLys[0] = std::make_unique<TrafikkLys>(RECT{ (center.x - roadWidth / 2),(center.y - roadWidth / 2),(center.x - roadWidth / 2),(center.y + roadWidth / 2) }, TrafikkLys::Tilstand::RED);
+        trafikkLys[1] = std::make_unique<TrafikkLys>(RECT{ (center.x + roadWidth / 2), (center.y - roadWidth / 2), (center.x + roadWidth / 2), (center.y + roadWidth / 2) }, TrafikkLys::Tilstand::GREEN);
 
-        trafikkLys[0] = std::make_unique<TrafikkLys>(RECT{ (centerX - spacing / 2 - roadWidth),(centerY - 100 / 2),(centerX - spacing / 2),(centerY + roadWidth / 2) }, TrafikkLys::Tilstand::RED);
-        trafikkLys[1] = std::make_unique<TrafikkLys>(RECT{ (centerX + spacing / 2), (centerY - roadWidth / 2), (centerX + spacing / 2 + roadWidth), (centerY + roadWidth / 2) }, TrafikkLys::Tilstand::GREEN);
+        SetTimer(hWnd, TIMER_LYS, 2000, NULL);
+        SetTimer(hWnd, TIMER_BIL, 100, NULL);
+        SetTimer(hWnd, TIMER_ADD_BILER, 500, NULL);
 
-        SetTimer(hWnd, TIMER_LYS, 1000, NULL);
-        SetTimer(hWnd, TIMER_BIL, 1000, NULL);
-        SetTimer(hWnd, TIMER_DRIVE, 10, NULL);
-        SetTimer(hWnd, TIMER_ADD_BILER, 1000, NULL);
     }
     break;
 
@@ -209,38 +194,43 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
     case WM_PAINT:
     {
         PAINTSTRUCT ps;
+
         HDC phdc = BeginPaint(hWnd, &ps);
         HDC hdc = CreateCompatibleDC(phdc);
+
         //henter vindue stoerelse
         RECT clientRect;
         GetClientRect(hWnd, &clientRect);
         //lager bitmap foer alt anna for at den ikke tegner bakgrunn over alt sammen
         HBITMAP bmp = CreateCompatibleBitmap(phdc, clientRect.right, clientRect.bottom);
-        SelectObject(hdc, bmp);
+        SelectObject(phdc, bmp);
         //ny bitmap farge (for bil veien)
         HBRUSH bg = CreateSolidBrush(RGB(200, 200, 200));
-        HGDIOBJ hOrg = SelectObject(hdc, bg);
-        Rectangle(hdc, 0, 0, clientRect.right, clientRect.bottom);
-        //int circleX = 0;
-        int circleY = (clientRect.bottom) / 2;
-        //skole buss broom broom
-        HBRUSH hYellowBrush = CreateSolidBrush(RGB(255, 255, 0));
-        HBRUSH hWheelBrush = CreateSolidBrush(RGB(128, 128, 128));
-        SelectObject(hdc, hWheelBrush);
-        Ellipse(hdc, circleX - 115, circleY, circleX-90, circleY+35);
-        Ellipse(hdc, circleX - 40, circleY, circleX - 15, circleY + 35);
-        SelectObject(hdc, hYellowBrush);
-        Rectangle(hdc, circleX - 125, circleY + 25, circleX, circleY - 25);
-        HBRUSH hBlueBrush = CreateSolidBrush(RGB(135, 206, 235));
-        SelectObject(hdc, hBlueBrush);
-        Rectangle(hdc, circleX - 30, circleY + 22, circleX - 5, circleY - 22);
-        RECT rectangle = { circleX - 125, circleY + 25, circleX - 34, circleY - 25 };
-        SelectObject(hdc, hYellowBrush);
-        LPCWSTR text = L"School Buss";
-        Rectangle(hdc, rectangle.left, rectangle.top, rectangle.right, rectangle.bottom);
-        SetBkMode(hdc, TRANSPARENT);
-        DrawText(hdc, text, -1, &rectangle, DT_SINGLELINE | DT_CENTER | DT_VCENTER);
-        SelectObject(hdc, hOrg);
+        HGDIOBJ hOrg = SelectObject(phdc, bg);
+        Rectangle(phdc, 0, 0, clientRect.right, clientRect.bottom);
+        //auto generer biler
+        auto itp = northCarsPosition.begin();
+        auto itc = northCarsColor.begin();
+        while (itp != northCarsPosition.end() && itc != northCarsColor.end()) {
+            HBRUSH carBrush = CreateSolidBrush(*itc);
+            SelectObject(phdc, carBrush);
+            Rectangle(phdc, center.x - bilBredde/2, *itp - bilLengde / 2, center.x + bilBredde/2, *itp + bilLengde / 2);
+            SelectObject(phdc, hOrg);
+            DeleteObject(carBrush);
+            itp++;
+            itc++;
+        }
+        itp = westCarsPosition.begin();
+        itc = westCarsColor.begin();
+        while (itp != westCarsPosition.end() && itc != westCarsColor.end()) {
+            HBRUSH carBrush = CreateSolidBrush(*itc);
+            SelectObject(phdc, carBrush);
+            Rectangle(phdc, *itp - bilLengde/2, center.y - bilBredde / 2, *itp+bilLengde/2, center.y + bilBredde / 2);
+            SelectObject(phdc, hOrg);
+            DeleteObject(carBrush);
+            itp++;
+            itc++;
+        }
         //bygger bitmap
         BitBlt(phdc, 0, 0, clientRect.right, clientRect.bottom, hdc, 0, 0, SRCCOPY);
         //fortau
@@ -248,10 +238,14 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
         //trafikkLys lys
         if (trafikkLys[0]) trafikkLys[0]->tegn(phdc);
         if (trafikkLys[1]) trafikkLys[1]->tegn(phdc);
+        //Prosenter
+        WCHAR num[10];
+        SetBkMode(phdc, TRANSPARENT);
+        StringCbPrintf(num, 10, L"%d%%", pW);
+        TextOut(phdc, center.x - roadWidth+7,center.y + roadWidth+50, num, wcsnlen_s(num, 10));
+        StringCbPrintf(num, 10, L"%d%%", pN);
+        TextOut(phdc, center.x + roadWidth / 2 + 7, center.y - 165, num, wcsnlen_s(num, 10));
         //cleanup
-        DeleteObject(hYellowBrush);
-        DeleteObject(hWheelBrush);
-        DeleteObject(hBlueBrush);
         DeleteObject(bmp);
         DeleteDC(hdc);
         DeleteObject(bg);
@@ -262,156 +256,133 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
 
     case WM_TIMER:
     {
-        static int timerCount = 0;
         switch (wParam)
         {
         case TIMER_LYS:
-            //lightState = ++lightState % 4;
+        {
             trafikkLys[0]->SkiftTilstand();
             trafikkLys[1]->SkiftTilstand();
 
-            if (timerCount < 1)
+            if (trafikkLys[0]->tilstand == TrafikkLys::Tilstand::GREEN || trafikkLys[1]->tilstand == TrafikkLys::Tilstand::GREEN)
             {
-                SetTimer(hWnd, TIMER_LYS, 1000, NULL);
-                timerCount++;
+                SetTimer(hWnd, TIMER_LYS, 3000, NULL);
             }
             else
             {
-                SetTimer(hWnd, TIMER_LYS, 3000, NULL);
-                timerCount = 0;
+                SetTimer(hWnd, TIMER_LYS, 1000, NULL);
             }
             InvalidateRect(hWnd, NULL, TRUE);
-            break;
-
-        case TIMER_BIL:
-
-            RECT clientRect;
-            GetClientRect(hWnd, &clientRect);
-            int centerX = (clientRect.right) / 2;
-            int centerY = (clientRect.bottom) / 2;
-
-            int prePos = 99999; //for at ingen biler er synlig paa starten
-            int minDistanse = 10; //minimum distanse mellom biler
-            int speed = 10; //hastighet paa biler
-            int bilLengde = 25;
-            int roadWidth = 50;
-
-            /* not worki
-            auto pos = northCarsPosition.begin();
-            while (pos != northCarsPosition.end()) {
-                if (*pos + minDistanse + speed + bilLengde < prePos) {
-                    if (lightstate != 2 && *pos + bilLengde <= roadWidth/-2 && (*pos + bilLengde + speed >= roadWidth/-2)) {
-                        (*pos) = roadWidth/-2-bilLengde;
-                    }else {
-                    (*pos) += speed;
-                }
-            }
-            prePos = *pos;
-            pos++;
-            }
-
-            pos = westCarsPosition.begin();
-            prePos = 99999;
-            while (pos != westCarsPosition.end()) {
-                if (*pos + minDistanse + speed + bilLengde < prePos) {
-                    if (lightstate != 0 && *pos + bilLengde <= roadWidth/-2 && (*pos + bilLengde + speed >= roadWidth/-2)) {
-                        (*pos) = roadWidth/-2-bilLengde;
-                    }else {
-                    (*pos) += speed;
-                }
-            }
-            prePos = *pos;
-            pos++;
-            }
-            */
-            InvalidateRect(hWnd, NULL, TRUE);
-            break;
-        }
-    //case TIMER_DRIVE:
-            //rot = (rot+1) % 360;
-            circleX += 5; //speed of cars
-            InvalidateRect(hWnd, 0, false);
-            break;
-        
-        
-        /*
-        case TIMER_ADD_BILER:
-        HDC screenDC = GetDC(hWnd);
-        int XRes = GetDeviceCaps(screenDC, HORZRES);
-        int YRes = GetDeviceCaps(screenDC, VERTRES);
-        DeleteDC(screenDC);
-        if(northCarsColor.empty() || *(northCarsPosition.rbegin()) > -(YRes/2+50-bilLengde))........{
-            if(rand()%100<pN){
-                northCarsColor.push_back(RGB(rand()%255,rand()%255,radn()%255));
-                northCarsPosition.push_back(-(YRes/2+50));
-            }
-        }
-        //remove cars outside screen
-        if(!northCarsColor.empty() && *(northCarsPosition.begin())YRes/2+50){
-        northCarsPosition.pop_front();
-        northCarsColor.pop_front();
-        }
-        if (westCarsColor.empty() && *(westCarsPosition.rbegin()) > -(XRes/2+50-bilLengde)){
-            if (rand()%100<pW){
-                westCarsColor.push_back(RGB(rand()%255,rand()%255,radn()%255));
-                westCarsPosition.push_back(-(XRes/2+50));
-            }
-        }
-        if(!westCarsColor.empty() && *(westCarsPosition.begin())>XRes/2+50){
-            westCarsPosition.pop_front();
-            westCarsColor.pop_front();
         }
         break;
-    }
-        */
+
+        case TIMER_BIL:
+        {
+            int prePos = 99999; //for at ingen biler er synlig paa starten
+            int minDistanse = 5; //minimum distanse mellom biler
+            int speed = 20; //hastighet paa biler
+
+            auto it = northCarsPosition.begin();
+            while (it != northCarsPosition.end()) {
+                if (*it + minDistanse + speed + bilLengde < prePos) {
+                    if (trafikkLys[1]->tilstand != TrafikkLys::Tilstand::GREEN
+                       && *it + bilLengde <= center.y - 25) {
+                        (*it) += 0;
+                    }
+                    else {
+                        (*it) += speed;
+                    }
+                }
+                prePos = *it;
+                it++;
+            }
+
+            prePos = 99999;
+            it = westCarsPosition.begin();
+            while (it != westCarsPosition.end()) {
+                if (*it + minDistanse + speed + bilLengde < prePos) {
+                    if (trafikkLys[0]->tilstand != TrafikkLys::Tilstand::GREEN
+                        && *it + bilLengde <= center.x-25) {
+                        (*it) += 0;
+                    }
+                    else {
+                        (*it) += speed;
+                    }
+                }
+                prePos = *it;
+                it++;
+            }
+
+            InvalidateRect(hWnd, NULL, TRUE);
+        }
+        break;
+
+        case TIMER_ADD_BILER:
+        {
+            HDC screenDC = GetDC(hWnd);
+            int XRes = GetDeviceCaps(screenDC, HORZRES);
+            int YRes = GetDeviceCaps(screenDC, VERTRES);
+            DeleteDC(screenDC);
+            //spawn top biler
+            if (northCarsColor.empty() || *(northCarsPosition.rbegin()) > -(YRes / 2 + 50 - bilLengde)) {
+                if (rand() % 100 < pN) {
+                    northCarsColor.push_back(RGB(rand() % 255, rand() % 255, rand() % 255));
+                    northCarsPosition.push_back(-(YRes / 2 + 50));
+                }
+            }
+            //slett top biler
+            if (!northCarsColor.empty() && *(northCarsPosition.begin()) > clientRect.bottom - bilLengde / 2) {
+                northCarsPosition.pop_front();
+                northCarsColor.pop_front();
+            }
+            //spawn venstre biler
+            if (westCarsColor.empty() || *(westCarsPosition.rbegin()) > -(XRes/2+50-bilLengde)) {
+                if (rand() % 100 < pW) {
+                    westCarsColor.push_back(RGB(rand() % 255, rand() % 255, rand() % 255));
+                    westCarsPosition.push_back(-(XRes/2+50));
+                }
+            }
+            //slett venstre biler
+            if (!westCarsColor.empty() && *(westCarsPosition.begin()) > clientRect.right - bilLengde/2) {
+                westCarsPosition.pop_front();
+                westCarsColor.pop_front();
+            }
+        }
+        break;
+        }
         break;
     }
     case WM_KEYDOWN:
         switch (wParam) {
         case VK_LEFT:
-            pW = pW >= 5 ? pW - 5 : 0;
+            pW = pW >= 10 ? pW - 10 : 0;
             break;
         case VK_RIGHT:
-            pW = pW <= 95 ? pW + 5 : 100;
+            pW = pW <= 90 ? pW + 10 : 100;
             break;
         case VK_DOWN:
-            pN = pN >= 5 ? pN - 5 : 0;
+            pN = pN >= 10 ? pN - 10 : 0;
             break;
         case VK_UP:
-            pN = pN <= 95 ? pN + 5 : 100;
+            pN = pN <= 90 ? pN + 10 : 100;
             break;
         }
         break;
-
     case WM_DESTROY:
         PostQuitMessage(0);
         break;
-    case WM_LBUTTONDOWN:
-        lightstate = ++lightstate % 4;
-        InvalidateRect(hWnd, 0, 0);
 
     case WM_SIZE:
     {
         int width = LOWORD(lParam);  // Ny bredde på vinduet
         int height = HIWORD(lParam); // Ny høyde på vinduet
 
-        RECT clientRect;
-        GetClientRect(hWnd, &clientRect);
-        int centerX = clientRect.right / 2;
-        int centerY = clientRect.bottom / 2;
-        int spacing = 100;
-        int roadWidth = 50;
-
         // Oppdaterer posisjonene til trafikklysenes rektangler
-        trafikkLys[0]->oppdaterPosisjon(RECT{ centerX - 100, centerY + 50, centerX - 50, centerY + 150 });
-
-        //trafikkLys[0]->oppdaterPosisjon(RECT{ centerX - spacing / 2 - roadWidth, centerY - roadWidth / 2, centerX - spacing / 2, centerY + roadWidth / 2 });
-        trafikkLys[1]->oppdaterPosisjon(RECT{ centerX + spacing / 2, centerY - 150, centerX + spacing / 2 + roadWidth, centerY - 50 });
+        trafikkLys[0]->oppdaterPosisjon(RECT{ center.x - roadWidth, center.y + roadWidth/2, center.x - roadWidth/2, center.y + 150 });
+        trafikkLys[1]->oppdaterPosisjon(RECT{ center.x + roadWidth / 2, center.y - 150, center.x + roadWidth, center.y - 50 });
 
         InvalidateRect(hWnd, NULL, TRUE); // Be om at vinduet tegnes på nytt
     }
     break;
-
     default:
         return DefWindowProc(hWnd, message, wParam, lParam);
     }
